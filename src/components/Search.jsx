9911cf62a4fa4b9542c93a3c3442b5ad33b538a3
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-04 10:45:40 -0500

    Test commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import API from "../utils/API";
-import "./search.css";
+// import React from "react";
+// import API from "../utils/API";
+// import "./search.css";
 
 
-render() {
-    return (
-        <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Fluid jumbotron</h1>
-    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-  </div>
-</div>
-    );
-  }
+// render() {
+//     return (
+//         <div class="jumbotron jumbotron-fluid">
+//   <div class="container">
+//     <h1 class="display-4">Fluid jumbotron</h1>
+//     <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+//   </div>
+// </div>
+//     );
+//   }
