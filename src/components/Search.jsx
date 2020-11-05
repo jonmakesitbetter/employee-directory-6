2fb5df118ee9c3456af4e0cc3ac4cc3c82091a6e
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-05 15:54:40 -0500

    Fixed type-o in handleInputChange function and added to render method, tested. Page works now, styling next

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,61 +2,84 @@ import React, { Component } from "react";
 import API from "../utils/API";
 import Table from "./Table";
 
-
-
-
 class Search extends Component {
+  state = { employees: [], searchTerm: "", filteredEmployees: [] };
 
-    state = { employees: [], searchTerm: "", filteredEmployees: [] }
-
-    componentDidMount() {
-        API.getEmployees()
-            .then(res => {
-                console.log(res);
-                this.setState({ employees: res.data.results, filteredEmployees: res.data.results })
-            }).catch(err => (console.log(err)));
-    }
-    handleInputChange = (event) => {
+  componentDidMount() {
+    API.getEmployees()
+      .then((res) => {
+        console.log(res);
         this.setState({
-            searchTerm: event.target.value
-        }, () => {
-            if (this.state.searchTerm === "") {
-                this.setState({ filteredEmployees: this.state.employees });
-            } else {
-                const searchedEmployee = this.state.employees.filter((employee) => {
-                    return employee.name.first.slice(0, this.state.searchTerm.length).tolowerCase() === this.state.searchTerm.toLowerCase()
-                })
-                this.setState({ filteredEmployees: searchedEmployee })
-            }
-        })
-    }
-
-    handleSort = () => {
-        var employeeArray = this.props.employees;
-        employeeArray.sort((a, b) => {
-            if (a.name.first < b.name.first) return -1;
-            if (a.name.first > b.name.first) return 1;
-            return 0;
-        })
-
-        this.setState({
-            filterData: employeeArray
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
         });
-    };
+      })
+      .catch((err) => console.log(err));
+  }
+  handleInputChange = (event) => {
+    this.setState(
+      {
+        searchTerm: event.target.value,
+      },
+      () => {
+        if (this.state.searchTerm === "") {
+          this.setState({ filteredEmployees: this.state.employees });
+        } else {
+          const searchedEmployee = this.state.employees.filter((employee) => {
+            return (
+              employee.name.first
+                .slice(0, this.state.searchTerm.length)
+                .toLowerCase() === this.state.searchTerm.toLowerCase()
+            );
+          });
+          this.setState({ filteredEmployees: searchedEmployee });
+        }
+      }
+    );
+  };
 
-    render() {
-        return (
-            <div>
-            <div class="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 class="display-4">Fluid jumbotron</h1>
-                    <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                </div>
-           </div>
-           <Table employees={this.state.filteredEmployees} handleSort={this.handleSort}/>
-           </div>
-        );
-    }
+  handleSort = () => {
+    var employeeArray = this.state.employees;
+    employeeArray.sort((a, b) => {
+      if (a.name.first < b.name.first) return -1;
+      if (a.name.first > b.name.first) return 1;
+      return 0;
+    });
+
+    this.setState({
+      filterData: employeeArray,
+    });
+  };
+
+
+  render() {
+    return (
+      <div>
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">
+              This is a modified jumbotron that occupies the entire horizontal
+              space of its parent.
+            </p>
+
+            <label for="exampleInputPassword1">Search</label>
+            <input 
+              type="input"
+              class="form-control"
+              name="employee-search"
+              value={this.state.searchTerm}
+              onChange={this.handleInputChange}
+            />
+          </div>
+        </div>
+        <Table
+          employees={this.state.filteredEmployees}
+          handleSort={this.handleSort}
+        />
+      </div>
+    );
+  }
 }
 
 export default Search;
--- src/utils/API.jsx
+++ src/utils/API.jsx
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default {
     getEmployees: ()=>{
-        return axios.get("https://randomuser.me/api/?results=15@nat=us")
+        return axios.get("https://randomuser.me/api/?results=15&nat=us")
     }
 }
