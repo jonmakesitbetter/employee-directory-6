d3cfe9eacebd053fb0c44c628311737ffd532644
Jon Peach <jonmakesitbetter@gmail.com>
2020-11-05 09:07:03 -0500

    Wrote name-sorting function, commented out for table rendering test, wrote table skeleton, tested. Will test sorting function once variables are in place in the table for dynamic employee generation

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,8 +2,51 @@ import React, { Component } from "react";
 
 class Table extends Component {
 
+
+//     handleSort = () => {
+//     var employeeArray = this.props.employee;
+//     employeeArray.sort((a, b)=> {
+//         if (a.name.first < b.name.first) return -1;
+//         if (a.name.first > b.name.first) return 1;
+//         return 0;
+//     })
+
+//     this.setState.employeeArray({
+//         filterData: employeeArray
+//     });
+// }
+
 render(){
-  return <h1>Table</h1>
+  return <table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">Portrait</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Email</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 }
 
 }
